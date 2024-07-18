ee305243a7ea6c81995f1575806b71a9d01c5e63
khalatesonalilive <khalatesonali1234@gmail.com>
2024-07-18 13:49:33 +0530

    PIMPage TC Written by the Rahul

---
+++ src/main/java/Tutorial3/Demo3.java
@@ -0,0 +1,11 @@
+package Tutorial3;
+
+public class Demo3 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("PIMPage Test Cases Written by Rahul Automation Tester");
+
+	}
+
+}
